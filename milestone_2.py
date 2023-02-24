cfde09027cbba9b61739ab022cfef707b99516dd
herdataspace <stephaniehunter01@gmail.com>
2023-02-24 14:23:49 +0000

    Adding conditional checks for user input

--- milestone_2.py
+++ milestone_2.py
@@ -4,3 +4,8 @@ word = random.choice(word_list)
 print(word)
 
 guess = input("Enter a single letter")
+
+if len(guess) == 1 and guess.isalpha():
+    print('Good guess!')
+else:
+    print('Oops! That is not a valid input.')
